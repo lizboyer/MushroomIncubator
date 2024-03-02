400369c488a1226a14cb9e5643227648894cc190
lizboyer <lizboyer01@gmail.com>
2024-03-02 16:03:14 -0600

    Temp/Humidity Displaying

--- graphicstest/graphicstest.ino
+++ graphicstest/graphicstest.ino
@@ -37,6 +37,10 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+
 #if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
   #define TFT_CS         14
   #define TFT_RST        15
@@ -78,109 +82,104 @@ Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 // OR for the ST7789-based displays, we will use this call
 //Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
+#include <Wire.h>
 
 float p = 3.1415926;
+
+bool enableHeater = false;
+uint8_t loopCnt = 0;
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 void setup(void) {
   Serial.begin(9600);
   Serial.print(F("Hello! ST77xx TFT Test"));
 
-  // Use this initializer if using a 1.8" TFT screen:
-//  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-
-  // OR use this initializer if using a 1.8" TFT screen with offset such as WaveShare:
-  // tft.initR(INITR_GREENTAB);      // Init ST7735S chip, green tab
-
-  // OR use this initializer (uncomment) if using a 1.44" TFT:
-  //tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
-
-  // OR use this initializer (uncomment) if using a 0.96" 160x80 TFT:
-  //tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
-  // OR use this initializer (uncomment) if using a 0.96" 160x80 TFT with 
-  // plug-in FPC (if you see the display is inverted!)
-  //tft.initR(INITR_MINI160x80_PLUGIN);  // Init ST7735S mini display
-
-  // OR use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
-  //tft.init(240, 240);           // Init ST7789 240x240
-
-  // OR use this initializer (uncomment) if using a 1.69" 280x240 TFT:
-  //tft.init(240, 280);           // Init ST7789 280x240
-
-  // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
-  //tft.init(240, 320);           // Init ST7789 320x240
-
-  // OR use this initializer (uncomment) if using a 1.14" 240x135 TFT:
+  // use this initializer (uncomment) if using a 1.14" 240x135 TFT:
   tft.init(135, 240);           // Init ST7789 240x135
-  
-  // OR use this initializer (uncomment) if using a 1.47" 172x320 TFT:
-  //tft.init(172, 320);           // Init ST7789 172x320
 
   // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
   // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
   // may end up with a black screen some times, or all the time.
   //tft.setSPISpeed(40000000);
 
-  Serial.println(F("Initialized"));
+  // Temp sensor setup
+  Serial.begin(9600);
 
-  uint16_t time = millis();
-  tft.fillScreen(ST77XX_BLACK);
-  time = millis() - time;
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println(time, DEC);
-  delay(500);
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
 
-  // large block of text
-  tft.fillScreen(ST77XX_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
-  delay(1000);
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
 
-  // tft print function!
-  tftPrintTest();
-  delay(4000);
-
-//  // a single pixel
-//  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
-//  delay(500);
-//
-//  // line draw test
-//  testlines(ST77XX_YELLOW);
-//  delay(500);
-//
-//  // optimized lines
-//  testfastlines(ST77XX_RED, ST77XX_BLUE);
-//  delay(500);
-//
-//  testdrawrects(ST77XX_GREEN);
-//  delay(500);
-//
-//  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
-//  delay(500);
-//
+//  uint16_t time = millis();
 //  tft.fillScreen(ST77XX_BLACK);
-//  testfillcircles(10, ST77XX_BLUE);
-//  testdrawcircles(10, ST77XX_WHITE);
-//  delay(500);
-
-//  testroundrects();
+//  time = millis() - time;
+//
+//  Serial.println(time, DEC);
 //  delay(500);
 //
-//  testtriangles();
-//  delay(500);
-//
-//  mediabuttons();
-//  delay(500);
-//
-//  Serial.println("done");
+//  // large block of text
+//  tft.fillScreen(ST77XX_BLACK);
+//  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
 //  delay(1000);
+//
+//  // tft print function!
+//  tftPrintTest();
+//  delay(4000);
+
 }
 
 void loop() {
-  tftPrintTest();
-  delay(4000);
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+
+  delay(1000);
+
+  // Toggle heater enabled state every 30 seconds
+  // An ~3.0 degC temperature increase can be noted when heater is enabled
+  if (loopCnt >= 30) {
+    enableHeater = !enableHeater;
+    sht31.heater(enableHeater);
+    Serial.print("Heater Enabled State: ");
+    if (sht31.isHeaterEnabled())
+      Serial.println("ENABLED");
+    else
+      Serial.println("DISABLED");
+
+    loopCnt = 0;
+  }
+  loopCnt++;
+  
+  tftPrintTest(t, h);
+
+
 //  tft.invertDisplay(true);
 //  delay(500);
 //  tft.invertDisplay(false);
 //  delay(500);
+
 }
 
 void testlines(uint16_t color) {
@@ -312,43 +311,43 @@ void testroundrects() {
   }
 }
 
-void tftPrintTest() {
+void tftPrintTest(float t, float h) {
   tft.setTextWrap(false);
   tft.fillScreen(ST77XX_BLACK);
   tft.setCursor(0, 30);
   tft.setTextColor(ST77XX_RED);
-  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(2);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_GREEN);
   tft.setTextSize(3);
-  tft.println("Hello World!");
-  tft.setTextColor(ST77XX_BLUE);
-  tft.setTextSize(4);
-  tft.print(1234.567);
+  tft.println("Temp *C = "); tft.println(t); tft.println("\t\t");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(3);
+  tft.println("Hum. % = "); tft.println(h); tft.println("\t\t");
+//  tft.setTextColor(ST77XX_GREEN);
+//  tft.setTextSize(3);
+//  tft.println("Hello World!");
+//  tft.setTextColor(ST77XX_BLUE);
+//  tft.setTextSize(4);
+//  tft.print(1234.567);
   delay(1500);
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(0);
-  tft.println("Hello World!");
-  tft.setTextSize(1);
-  tft.setTextColor(ST77XX_GREEN);
-  tft.print(p, 6);
-  tft.println(" Want pi?");
-  tft.println(" ");
-  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
-  tft.println(" Print HEX!");
-  tft.println(" ");
-  tft.setTextColor(ST77XX_WHITE);
-  tft.println("Sketch has been");
-  tft.println("running for: ");
-  tft.setTextColor(ST77XX_MAGENTA);
-  tft.print(millis() / 1000);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.print(" seconds.");
+//  tft.setCursor(0, 0);
+//  tft.fillScreen(ST77XX_BLACK);
+//  tft.setTextColor(ST77XX_WHITE);
+//  tft.setTextSize(0);
+//  tft.println("Hello World!");
+//  tft.setTextSize(1);
+//  tft.setTextColor(ST77XX_GREEN);
+//  tft.print(p, 6);
+//  tft.println(" Want pi?");
+//  tft.println(" ");
+//  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+//  tft.println(" Print HEX!");
+//  tft.println(" ");
+//  tft.setTextColor(ST77XX_WHITE);
+//  tft.println("Sketch has been");
+//  tft.println("running for: ");
+//  tft.setTextColor(ST77XX_MAGENTA);
+//  tft.print(millis() / 1000);
+//  tft.setTextColor(ST77XX_WHITE);
+//  tft.print(" seconds.");
 }
 
 void mediabuttons() {
